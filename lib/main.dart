ce4c6de4d3b0d866427548616c528ead1182cc0c
Aleksander Panarin <dudefluch@gmail.com>
2022-03-17 10:18:31 +0200

    Change main title

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Canvas',
+      title: 'Flutter Puzzle Hack',
       home: MainMenuPage(),
     );
   }
